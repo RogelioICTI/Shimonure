38b421ee9b0bb7c1596c14eb7eabb8402be3786b
Rogelio <rogelio@dsindigo.mx>
2016-03-29 18:20:09 -0600

    Avances

--- header.php
+++ header.php
@@ -14,7 +14,9 @@ $url_base = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
         <link href="css/extras.css" rel="stylesheet" />
 
         <link href='https://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Dosis' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        
         <title>Rogelio Vargas Marquez | Shimonure</title>
         
         <link rel="shortcut icon" type="image/png" href="favicon.png" />
--- pages/home/about_me.php
+++ pages/home/about_me.php
@@ -3,12 +3,17 @@
         <div class="col-md-8 col-sm-12">
             <h1 class="padding-bottom-15">Hola :)</h1>
             <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id malesuada dui, vel rhoncus diam. Nunc a justo lectus. Etiam vulputate metus non sodales venenatis. Donec placerat lacinia erat sit amet laoreet. Morbi id erat libero. Fusce nec mauris nec justo finibus vehicula. Maecenas eget odio euismod, feugiat elit id, gravida leo. Nam vitae nunc ut eros pretium tincidunt volutpat at lacus. Morbi et turpis nulla. Nunc tincidunt dapibus dui.</p>
+            <div class="margin-top-20">
+                <a class="btn btn-primary fs-18 ">Información sobre mi</a>
+            </div>
         </div>
         <div class="col-md-4 text-center hidden-sm hidden-xs">
             <img src="http://fpoimg.com/300x300?text=Fotografía">
         </div>
         
         <!--<div style="height: 400px" align="center"><h1 >About Me</h1></div>-->
+        
     </div>
 </div>
+
 <div class="division-codigo"></div>
